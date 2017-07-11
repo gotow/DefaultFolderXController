91ddc784e8f33d694803989adadc410ab7cd4344
gotow <gotow@stclairsoft.com>
2017-07-10 19:22:13 -0600

    Switched from AppleScript to private pasteboard
    Sigh - switched to messaging via a private pasteboard because the sandbox's AppleScript access-group entitlements don't work reliably. Damn you, Apple.

--- DefaultFolderXController.m
+++ DefaultFolderXController.m
@@ -6,31 +6,26 @@
 //
 
 #import "DefaultFolderXController.h"
-#import <ScriptingBridge/ScriptingBridge.h>
 
-// Declare these to shut the compiler up
-@interface SBApplication()
-- (void)Enable;
-- (void)Disable;
-@end
-
-// A one-method singleton
 @implementation DefaultFolderXController
+
+static NSString *dfxBundleID = @"com.stclairsoft.DefaultFolderX5";
+static NSString *dfxEnablePboardType = @"EnableFlag";
 
 //-------------------------------------------------------------------------------------------------------------
 // Enable or disable Default Folder X. This does nothing if Default Folder X isn't running.
 + (BOOL)setEnabled:(BOOL)enabled
 {
-    static NSString *dfxBundleID = @"com.stclairsoft.DefaultFolderX5";
     BOOL result = NO;
     
     if([NSRunningApplication runningApplicationsWithBundleIdentifier:dfxBundleID].count > 0) {
-        SBApplication *dfx = [SBApplication applicationWithBundleIdentifier:dfxBundleID];
+        NSPasteboard *pb = [NSPasteboard pasteboardWithName:dfxBundleID];
+        [pb declareTypes:@[dfxEnablePboardType] owner:nil];
         if(enabled) {
-            [dfx Enable];
+            [pb setString:@"YES" forType:dfxEnablePboardType];
         }
         else {
-            [dfx Disable];
+            [pb setString:@"NO" forType:dfxEnablePboardType];
         }
         result = YES;
     }
